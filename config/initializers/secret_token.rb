825b0b3f83a8d155b700b3cec52e541c58ec702b
madebydna <info@madebydna.com>
2012-11-14 11:27:12 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MetaboDB::Application.config.secret_token = '5d5b4db3ce711fbfa93fe4d091c3c3723c05d97cbbd65bd4519e3df656832085ceadd2699bd8d647f4a600ceb9a38e52e20ca3edc5d575d819068ba633dcb75e'
